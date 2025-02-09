590fcdd076530ea344f0bb9cf8f187070a17e1eb
Saurabrai69 <saurabrai69@gmail.com>
2025-02-09 18:01:47 +0545

    learn about js

---
+++ js/index.js
@@ -0,0 +1,38 @@
+console.log("hello world");
+
+//es5
+var name = "saurab";
+var name = "shyam";
+console.log(name);
+
+//ES6
+// let, const
+
+const person = "saurab";
+console.log(person);
+
+let school = "abc";
+school = "xyz";
+console.log(school);
+
+//var, let, const
+
+//alert("hello world");
+//const user = prompt("what is your name");
+//console.log(user);
+
+const isStudent = confirm("are you above 18");
+console.log(isStudent);
+
+alert("welcome saurab");
+const user = prompt("what is your name");
+console.log(user);
+
+alert(user);
+
+//1
+const greeter = "welcome back," + user;
+alert(greeter);
+
+//2 String Literal
+alert(`welcome back, ${user}`);
