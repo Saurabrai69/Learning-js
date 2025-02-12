d408a1f1ef35dbabd59a92abb0f26727ae91d8c8
Saurabrai69 <saurabrai69@gmail.com>
2025-02-12 18:03:51 +0545

    learn about loop

---
+++ js/loops.js
@@ -0,0 +1,30 @@
+// Loop
+// Executing same logic over and over
+
+// For
+// do...while
+// While...
+
+// syntax
+// for(start;check;increment/decriment){}
+
+//Multiplication
+
+// for (let i = 1; i <= 10; i++) {
+//   console.log(`2*${i}=${2 * i}`);
+// }
+
+// Sum Of 1 to 100
+
+for (let sum = 0; s <= 100; s++) {
+  sum += s;
+}
+console.log(sum);
+
+
+// do while
+//do{}while()
+let k = 1;
+do{
+    console.log
+}
