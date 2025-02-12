e56f74189b10bbbe663b916fa837d93e0c4bd21f
Saurabrai69 <saurabrai69@gmail.com>
2025-02-12 22:27:39 +0545

    learn of operator

---
+++ js/operator.js
@@ -0,0 +1,34 @@
+// Logical Operator
+//3
+
+// AND
+// OR
+// NOT
+
+//AND
+// condotion 1 + condotion 2
+
+// const age = +prompt("age");
+// const dl = conform("do you have DL?");
+
+// const isAllowed = age >= 18 && dl === true ? "you are invited" : "sorry";
+// alert(isAllowed);
+
+//OR
+// condition 1  || conditin 2
+
+// ask user if they are allowed to vate based on nagarita or voter card?
+// alert their eligibility
+
+// const nagerita = confirm("do you have nagerita");
+// const voterCard = confirm("do you have voter card");
+// const isEligilible = nagarita === true || voterCard === true;
+
+// const eligible =
+// nagerita || voterCard ? "you are eligible" : "you are eligible";
+
+// NOT
+// const user = prompt("what is your name?"); // null, "", false, undefined
+// if (!user) {
+//   alert("User name is missing");
+// }
