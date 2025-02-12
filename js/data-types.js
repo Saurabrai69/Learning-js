45bea962592fa00aed27fa20a31c09b4e088023a
Saurabrai69 <saurabrai69@gmail.com>
2025-02-12 13:11:51 +0545

    data types

---
+++ js/data-types.js
@@ -0,0 +1,47 @@
+// // 1. single line comment(//)
+// // 2. multi line comment (/**/)
+
+// /*
+// premitive data types
+// string, number, Null, Boolean, Undefined
+
+// Comppex Data Types
+// Object
+// Object
+// arrey
+// Date
+
+// */
+// const string = "raktim shrestha is a good person."; //Character, Word, Sentence, Paragraph
+// const pi = 3.14;
+// let x;
+// const d = null;
+// const isMale = true;
+
+// Type Conversion / Type Coercion
+
+// const value = "13";
+// console.log(typesof value); //types checking
+
+// const actualNumber = Number(value); // type conversion
+// console.log(actualNumber);
+// console.log(typeof value);
+
+// //convert number to string
+// const num = 13;
+// const numToString = String(num);
+// console.log(num, numToString);
+
+// //Write a program that divides the two numbers asking user for the 2 numbers inputs and alert the result.
+const num1 = +prompt(" first number please");
+
+const num2 = prompt("second number please");
+console.log(typesof num2);
+const result = num1 / num2;
+alert(`The result of devision between ${num1} and ${num2} is ${result}`);
+
+//unary Operator 
+// let val = 1;
+// val++; //2
+// val += 1; //3
+// console.log(val);
